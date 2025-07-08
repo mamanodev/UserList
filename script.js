6b4b8b81e5a7ee66f13e13b8c99ee26a69558afa
mamanodev <ermaneveillard87@gmail.com>
2025-07-08 17:16:17 -0400

    commit -digning with bootstrap

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-async function loadUsers(){
-        const ul =document.getElementById("users");
-        ul.innerHTML="Changement..." ;
-        
-        try{
-            const response = await fetch("https://jsonplaceholder.typicode.com/users");
-            const users = await response.json();
+async function loadUsers() {
+    const ul = document.getElementById("users");
+    ul.innerHTML = "Changement...";
 
-            ul.innerHTML="";
-            users.forEach(user => {
-                const li = document.createElement("li");
-                li.textContent = `${user.name}  -  ${user.email}  -  ${user.address.city}`;
-                ul.appendChild(li);
-            });
-        } catch(err){
-            ul.innerHTML ="Erreur lors du chargement.";
-            console.error(err);
-        }
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const users = await response.json();
+
+        ul.innerHTML = "";
+        users.forEach(user => {
+            const li = document.createElement("li");
+            li.innerHTML = `
+              <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1 text-primary">${user.name}</h5>
+      <small class="badge text-bg-danger">${user.username}</small>
+    </div>
+    <p class="mb-1 text-success"> ${user.phone}</p>
+    <p class="mb-1"> ${user.email}    ${user.website}</p>
+    <p class="mb-1"> ${user.company.name}.</p>
+    <small class="text-warning">${user.address.city}  -   ${user.company.catchPhrase}</small>
+    <br>
+    <small class="text-warning-emphasis">${user.company.bs}</small>
+  </a>
+                 </p>
+ 
+</div>`;
+            ul.appendChild(li);
+        });
+    } catch (err) {
+        ul.innerHTML = "Erreur lors du chargement.";
+        console.error(err);
+    }
 }
